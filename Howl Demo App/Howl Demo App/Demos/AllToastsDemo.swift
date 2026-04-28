7bb117b4d565740ffa2060b17d3e876daebbc220
Ky <Ky@KyNorthstar.me>
2026-04-28 16:34:03 -0600

    Wrapping up rebranding

---
+++ Howl Demo App/Howl Demo App/Demos/AllToastsDemo.swift
@@ -0,0 +1,106 @@
+//
+//  AllToastsDemo.swift
+//  Howl Demo App
+//
+//  Created by Ky on 2026-04-23.
+//
+
+import SwiftUI
+
+
+
+struct AllToastsDemo: View {
+    
+    @State
+    private var selectedToast: DemoableToast = .bezel
+    
+    var body: some View {
+#if os(macOS)
+        identifiedToastView(for: selectedToast)
+            .toolbar(id: "Toasts") {
+                ToolbarItem(id: "Selected toast") {
+                    Picker("Selected toast", selection: $selectedToast) {
+                        ForEach(DemoableToast.allCases) {
+                            Text($0.rawValue.capitalized)
+                                .tag($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            }
+#elseif os(iOS)
+        TabView(selection: $selectedToast) {
+            ForEach(DemoableToast.allCases, id: \.id) { toast in
+                Tab(toast.localizedTitle, systemImage: toast.systemImage, value: toast) {
+                    identifiedToastView(for: toast)
+                }
+            }
+        }
+#endif
+    }
+}
+
+
+
+private extension AllToastsDemo {
+    func identifiedToastView(for tab: DemoableToast) -> some View {
+        toastView(for: tab)
+            .id(tab)
+            .tag(tab)
+    }
+    
+    
+    @ViewBuilder
+    func toastView(for tab: DemoableToast) -> some View {
+        switch tab {
+            case .bezel:
+            BezelToastDemo()
+        case .snackbar:
+            SnackbarToastDemo()
+        case .capsule:
+            CapsuleToastDemo()
+        }
+    }
+}
+
+
+
+private enum DemoableToast: String {
+    case bezel
+    case snackbar
+    case capsule
+}
+
+
+
+extension DemoableToast {
+    var localizedTitle: LocalizedStringKey {
+        switch self {
+        case .bezel:    "Bezel"
+        case .snackbar: "Snackbar"
+        case .capsule:  "Capsule"
+        }
+    }
+    
+    
+    var systemImage: String {
+        switch self {
+        case .bezel:    "square.inset.filled"
+        case .snackbar: "inset.filled.bottomleading.rectangle"
+        case .capsule:  "capsule"
+        }
+    }
+}
+
+
+
+extension DemoableToast: CaseIterable {}
+extension DemoableToast: Identifiable {
+    var id: Self { self }
+}
+
+
+
+#Preview {
+    AllToastsDemo()
+}
